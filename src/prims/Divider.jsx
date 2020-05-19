2f41dda46ad2ec280a77acc4427d818ee79534ff
royceshayegan <royceshayegan@gmail.com>
2020-05-19 17:27:31 -0400

    added optional chaining

--- src/prims/Divider.jsx
+++ src/prims/Divider.jsx
@@ -7,13 +7,13 @@ export default React.forwardRef((props, ref) => (
     {...props}
     __themeKey="dividers"
     __css={{
-      marginTop: theme => `${theme.dividers.root.marginTop || "18px"}`,
-      height: theme => `${theme.dividers.root.height || "3px"}`,
-      width: theme => `${theme.dividers.root.width || "224px"}`,
+      marginTop: theme => `${theme.dividers.root?.marginTop || "18px"}`,
+      height: theme => `${theme.dividers.root?.height || "3px"}`,
+      width: theme => `${theme.dividers.root?.width || "224px"}`,
       border: 0,
       color: "transparent",
       background: theme =>
-        `${theme.dividers.root.background || theme.colors.primary}`
+        `${theme.dividers.root?.background || theme.colors.primary}`
     }}
   />
 ));
